fdf1adaa33a7cfaeedf1f31ea39f454907a3b0bd
TheTechnobear <mharris@technobear.com>
2025-03-28 18:31:22 +0100

    move percmd to exec before synthor, only disable if S90percmd runs

--- scripts/release.ssp.sh
+++ scripts/release.ssp.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 echo SSP RELEASE : copy resourcs and build.ssp/PercCmd to release/ssp
 
-cp resources/S90perccmd releases/ssp
+cp resources/S80perccmd releases/ssp
 cp resources/perccmd.json releases/ssp
 cp resources/perccmd.sh.ssp releases/ssp/perccmd.sh
 cp build.ssp/PercCmd releases/ssp
--- scripts/release.xmx.sh
+++ scripts/release.xmx.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 echo XMX RELEASE : copy resourcs and build.xmx/PercCmd to release/xmx
 
-cp resources/S90perccmd releases/xmx
+cp resources/S80perccmd releases/xmx
 cp resources/perccmd.json releases/xmx
 cp resources/perccmd.sh.xmx releases/xmx/perccmd.sh
 cp build.xmx/PercCmd releases/xmx
